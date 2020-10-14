87b21c2acfce217cd00151bd3f8cfa464406863d
Benjamin Koehler <bkoehler751@gmail.com>
2020-10-14 10:32:04 -0700

    setup grid component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Grid from './components/grid/Grid'
 
 function App() {
   return (
     <div className="App">
      <h1>Game of Life</h1>
+     <Grid />
     </div>
   );
 }
---
+++ src/components/grid/Grid.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Grid = () => {
+    return (
+        <div>
+            <h1>Hello from grid</h1>
+        </div>
+    )
+}
+export default Grid
