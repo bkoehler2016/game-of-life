4d269fd1816dea6534dd3a9f87d84235dd30c7ff
Benjamin Koehler <bkoehler751@gmail.com>
2020-10-14 18:27:42 -0700

    setup components needed for canvas and started logic for render cells

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import './App.css';
-import Grid from './components/grid/Grid'
+import BoardGrid from './components/board/Board';
+
 
 function App() {
   
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#000000'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill()
-  }
+  
   
   return (
     <div className="App">
      <h1>Game of Life</h1>
-     <Grid draw= {draw}/>
+     <BoardGrid />
     </div>
   );
 }
