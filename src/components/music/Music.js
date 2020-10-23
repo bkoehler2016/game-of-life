e34e2d8850ccb944fb5321d212051a61a60cfba8
Benjamin Koehler <bkoehler751@gmail.com>
2020-10-22 19:55:18 -0700

    added play and stop buttons and got it work

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -3,12 +3,24 @@ import useSound from 'use-sound';
 import lifeMusic from './life.mp3';
 
 const Music = () => {
-  const [play] = useSound(lifeMusic); 
-  
- return(
-     <div>
-         <button className="music" onClick={play}>Play Music</button>
-     </div>
- )
-};
+    const [play, { stop }] = useSound(lifeMusic, {
+        interrupt: true
+    });
+    
+    const handleClick = () => {
+        play()
+    }
+    const stopPlay = () => {
+        stop()
+    }
+   
+    return (
+        <div>
+      <button className="music" onClick={handleClick} >
+       Play Music
+      </button>
+      <button className="music" onClick={stopPlay}>Stop Music</button>
+      </div>
+    );
+  };
 export default Music;
